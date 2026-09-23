94a5bf5412d651924d2d89196656f7e8b3276cb6
sidisnnew <laisiukai2013@gmail.com>
2026-09-23 20:52:31 +0800

    Create ticket model

--- djangoticket/ticket/models.py
+++ djangoticket/ticket/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ticket(models.Model):
+    t_title = models.CharField(max_length=100)
+    t_content = models.CharField(max_length=500)
+    pub_date = models.DateTimeField("Date published")
+    t_status = models.BooleanField(default=False)
