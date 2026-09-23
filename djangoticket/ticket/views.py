c75fa94411247944e54c8274bb61c1fc3729091e
sidisnnew <laisiukai2013@gmail.com>
2026-09-24 00:42:20 +0800

    view ver0.1

--- djangoticket/ticket/views.py
+++ djangoticket/ticket/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Ticket
 
 # Create your views here.
 def index(request):
     return HttpResponse("Index")
 
 def ticketlist(request):
-    return HttpResponse("List")
+    ticket_list = Ticket.objects.filter(t_status=False).order_by("pub_date")
+    output = ", ".join([t.t_title for t in ticket_list])
+    return HttpResponse(output)
 
 def allticketlist(request):
-    return HttpResponse("All")
+    all_ticket_list = Ticket.objects.order_by("-pub_date")
+    output = ", ".join(t.t_title for t in all_ticket_list)
+    return HttpResponse(output)
 
 def detail(request, t_id):
-    return HttpResponse("Detail %s" % t_id)
+    ticket = get_object_or_404(Ticket, pk=t_id)
+    output = ticket.t_content
+    return HttpResponse(output)
