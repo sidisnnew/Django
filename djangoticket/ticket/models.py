89470be72c9e68ac646cf45717645d4425786d44
sidisnnew <laisiukai2013@gmail.com>
2026-09-23 21:37:20 +0800

    Register ticket

--- djangoticket/ticket/models.py
+++ djangoticket/ticket/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class ticket(models.Model):
+class Ticket(models.Model):
     t_title = models.CharField(max_length=100)
     t_content = models.CharField(max_length=500)
     pub_date = models.DateTimeField("Date published")
     t_status = models.BooleanField(default=False)
+    t_warranty = models.BooleanField(null=True, blank=True, default=None)
+
+    def was_done(self):
+        return self.t_status
+
+    def __str__(self):
+        return self.t_title
