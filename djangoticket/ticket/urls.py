b5665b8b765514e98e19629d5b75bd4e29e53014
sidisnnew <laisiukai2013@gmail.com>
2026-09-25 02:27:42 +0800

    Add template and put them to views

--- djangoticket/ticket/urls.py
+++ djangoticket/ticket/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = "ticket"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:t_id>/", views.detail, name="detail"),
     path("tickets/", views.ticketlist, name="tickets"),
     path("all/", views.allticketlist, name="all"),
+    path("<int:t_id>/", views.detail, name="detail"),
 ]
--- djangoticket/ticket/views.py
+++ djangoticket/ticket/views.py
@@ -4,17 +4,20 @@ from .models import Ticket
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index")
+    ticket_number = Ticket.objects.filter(t_status=False).count()
+    all_ticket_number = Ticket.objects.count()
+    context = {"ticket_number":ticket_number, "all_ticket_number":all_ticket_number}
+    return render(request, "ticket/index.html", context)
 
 def ticketlist(request):
     ticket_list = Ticket.objects.filter(t_status=False).order_by("pub_date")
-    output = ", ".join([t.t_title for t in ticket_list])
-    return HttpResponse(output)
+    context = {"ticket_list":ticket_list}
+    return render(request, "ticket/ticket_list.html", context)
 
 def allticketlist(request):
     all_ticket_list = Ticket.objects.order_by("-pub_date")
-    output = ", ".join(t.t_title for t in all_ticket_list)
-    return HttpResponse(output)
+    context = {"all_ticket_list":all_ticket_list}
+    return render(request, "ticket/all_ticket_list.html", context)
 
 def detail(request, t_id):
     ticket = get_object_or_404(Ticket, pk=t_id)
